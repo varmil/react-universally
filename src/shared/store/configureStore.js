42d45f46575b3dc65807995cfd9609b0012b26ba
山本　晃大 <cf0293@CYGAMES-1818.local>
2016-09-02 12:15:02 +0900

    Add redux-devtools for Chrome setting

--- src/shared/store/configureStore.js
+++ src/shared/store/configureStore.js
@@ -1,7 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
 
+const enhancer = compose(
+  // Middleware you want to use in development:
+  // applyMiddleware(d1, d2, d3),
+  applyMiddleware(),
+  // Required! redux-devtoolsextension
+  // https://github.com/zalmoxisus/redux-devtools-extension
+  // TODO: remove on PRODUCTION build
+  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
+);
+
 export default (preloadedState) => {
-  const store = createStore(rootReducer, preloadedState);
+  const store = createStore(rootReducer, preloadedState, enhancer);
   return store;
 }
