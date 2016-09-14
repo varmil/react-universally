5659b17c81f055a3d7459765b782a747b7920a68
山本　晃大 <cf0293@CYGAMES-1818.local>
2016-09-14 16:13:15 +0900

    Implement star rating system

--- src/shared/components/FiveStar/FiveStar.js
+++ src/shared/components/FiveStar/FiveStar.js
@@ -4,21 +4,39 @@ import Star from 'material-ui/svg-icons/toggle/star';
 import { grey300, yellow700 } from 'material-ui/styles/colors';
 import styles from './index.css'
 
-const starStyle = {
-  width: 20,
-  height: 20,
+const DEFAULT_SIZE = 20
+
+function createLabel(props) {
+  if (props.noLabel) return null
+
+  // if rate can be changed, label does not show
+  if (! props.rating) return null
+
+  return (
+    <span className={`${styles.adjustRatingLine} ${styles.ratingLabel}`}>{(props.rating) ? props.rating.toFixed(2) : ''}</span>
+  )
 }
 
 export default (props) => {
+  const rating = (props.rating !== undefined) ? Number(props.rating) : undefined
+  const readonly = (props.readonly !== undefined) ? props.readonly : true
+  const starStyle = {
+    width: props.size || DEFAULT_SIZE,
+    height: props.size || DEFAULT_SIZE,
+  }
+
   return(
     <span>
       <StarRating
         empty={<Star style={starStyle} color={grey300} />}
         full={<Star style={starStyle} color={yellow700} />}
-        readonly={true}
-        initialRate={props.rating}
+        readonly={readonly}
+        initialRate={rating}
+        fractions={props.fractions}
+        onChange={props.onChange}
+        onRate={props.onRate}
       />
-      <span className={`${styles.adjustRatingLine} ${styles.ratingLabel}`}>{(props.rating) ? props.rating.toFixed(2) : ''}</span>
+      {createLabel(props)}
     </span>
   )
 }
--- src/shared/pages/Review/Edit/Top/Top.js
+++ src/shared/pages/Review/Edit/Top/Top.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
-import { isEmpty } from 'lodash'
+import { isEmpty, range, round } from 'lodash'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import Helmet from 'react-helmet'
 import Dropzone from 'react-dropzone'
+import { Flex, Box } from 'reflexbox'
 
-import { Divider, TextField } from 'material-ui'
+import { Divider, TextField, IconButton, SelectField, MenuItem } from 'material-ui'
 import ImageAddPhoto from 'material-ui/svg-icons/image/add-a-photo';
 
 import API from '../../../../api'
 import * as restaurantDetailActions from '../../../../actions/restaurantDetail'
 import * as errorsActions from '../../../../actions/errors'
+import FiveStar from '../../../../components/FiveStar'
 
 // TODO: とりあえずcommonデータを参照
 
@@ -19,7 +21,7 @@ const rstNameStyle = {
 }
 
 const containerMargin = {
-  margin: 20,
+  margin: 15,
 }
 
 const textFieldStyle = {
@@ -27,6 +29,44 @@ const textFieldStyle = {
   marginBottom: 0,
 }
 
+const iconStyles = {
+  small: {
+    width: 36,
+    height: 36,
+  },
+  medium: {
+    width: 48,
+    height: 48,
+  },
+  large: {
+    width: 60,
+    height: 60,
+  },
+}
+
+const iconButtonStyles = {
+  small: {
+    width: 72,
+    height: 72,
+    padding: 16,
+  },
+  medium: {
+    width: 96,
+    height: 96,
+    padding: 24,
+  },
+  large: {
+    width: 120,
+    height: 120,
+    padding: 30,
+  },
+}
+
+const dropzoneStyle = {
+  display: 'inline-block',
+  backgroundColor: 'whitesmoke',
+  border: '1px dashed gray',
+}
 
 class Top extends Component {
   static fetchData(query, params, dispatch) {
@@ -47,6 +87,11 @@ class Top extends Component {
 
 
 
+  constructor(props) {
+    super(props)
+    this.state = { rating: undefined }
+  }
+
   componentWillMount() {
     const { dispatch, common } = this.props
     const { location, params } = this.context
@@ -59,12 +104,25 @@ class Top extends Component {
 
 
 
+  onChangeRating(rate) {
+    // 文字列で渡ってくる場合もあるので
+    const strRate = (rate.toFixed) ? rate.toFixed(1) : rate
+    this.setState({ rating: strRate })
+  }
+
   onDrop(files) {
     console.log('Received files: ', files);
   }
 
 
-
+  createRatingMenuItem() {
+    return range(1.0, 5.1, 0.1).map(number => {
+      const rounded = round(number, 1).toFixed(1)
+      return (
+        <MenuItem key={rounded} value={rounded} primaryText={rounded} />
+      )
+    })
+  }
 
   render() {
     const { name } = this.props.common
@@ -76,7 +134,25 @@ class Top extends Component {
 
         <Divider />
 
-        ★★★★★
+        <div style={containerMargin}>
+          <Flex align="center" justify="flex-start" style={{}}>
+            <Box sm={10}>
+              <FiveStar readonly={false} fractions={2} size={38} onChange={::this.onChangeRating} rating={this.state.rating} noLabel={true} />
+            </Box>
+            <Box sm={2} ml={2}>
+              <SelectField
+                id="Top-SelectField-rating"
+                value={this.state.rating}
+                maxHeight={400}
+                style={{ width: 60 }}
+                labelStyle={{ fontSize: '22px', fontWeight: 'bold' }}
+                onChange={(e, i, rating) => this.onChangeRating(rating)}
+              >
+                {this.createRatingMenuItem()}
+              </SelectField>
+            </Box>
+          </Flex>
+        </div>
 
         <Divider />
 
@@ -88,8 +164,13 @@ class Top extends Component {
         />
 
         <div style={containerMargin}>
-          <Dropzone accept="image/*" onDrop={::this.onDrop}>
-            <div>Try dropping some files here, or click to select files to upload.</div>
+          <Dropzone accept="image/*" onDrop={::this.onDrop} style={dropzoneStyle}>
+            <IconButton
+              iconStyle={iconStyles.medium}
+              style={iconButtonStyles.medium}
+            >
+              <ImageAddPhoto />
+            </IconButton>
           </Dropzone>
         </div>
       </div>
