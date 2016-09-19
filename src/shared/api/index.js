96cf278d4fd1a63b66e170469262401f15bee998
Akihiro Yamamoto(home-mac) <ay@ay.local>
2016-09-19 18:14:58 +0900

    change api end point from localhost to .env

--- src/shared/api/index.js
+++ src/shared/api/index.js
@@ -15,7 +15,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:1337/api/',
+  baseURL: `http://${process.env.API_BASE_URL}:${process.env.SERVER_PORT}/api/`,
 })
 
 
