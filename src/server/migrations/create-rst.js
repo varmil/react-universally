48851be97b9f2c50668c6dec9f433a143ec67a5d
山本　晃大 <cf0293@CYGAMES-1818.local>
2016-09-26 11:25:21 +0900

    Create Rst Model

---
+++ src/server/migrations/create-rst.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Rsts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      link: {
+        type: Sequelize.STRING
+      },
+      low_budget: {
+        type: Sequelize.INTEGER
+      },
+      high_budget: {
+        type: Sequelize.INTEGER
+      },
+      open_hours: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      phone_number: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Rsts');
+  }
+};
---
+++ src/server/models/rst.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Rst = sequelize.define('Rst', {
+    name: DataTypes.STRING,
+    link: DataTypes.STRING,
+    low_budget: DataTypes.INTEGER,
+    high_budget: DataTypes.INTEGER,
+    open_hours: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phone_number: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Rst;
+};
