a0ee8c7894c5a1487e69cc59ae00c825b72e8e8e
ay-vagrant-home-desktop <fkmks247@gmail.com>
2017-01-14 10:12:59 +0900

    fix bug src/server/scripts/db_backup.sh

--- src/server/scripts/db_backup.sh
+++ src/server/scripts/db_backup.sh
@@ -2,12 +2,17 @@
 # http://qiita.com/Jey/items/b94958491bb6611a5666
 
 DATE=`date '+%d'`
-DUMPCMD="docker exec -it docker_mariadb_1 mysqldump -uroot"
+
 DB_NAME="foodbook_production"
 PASS="urpasswd"
+DUMPCMD="docker exec -i docker_mariadb_1 mysqldump -uroot"
+
+OUTPUT_DIR=~/dbbackup2/
+
+mkdir -p $OUTPUT_DIR
 
 $DUMPCMD \
-$DB_NAME | gzip > ~/dbbackup2/db-dump.${DATE}.sql.gz
+$DB_NAME | gzip > ${OUTPUT_DIR}db-dump.${DATE}.sql.gz
 
 # 一応echoって書いてるけど、hipchatとかに飛ばすとたのしい
-echo "dump completed"
+# echo "dump completed"
